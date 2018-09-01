c8cd72e2639df03ed85848c1a265411a211fb36b
Matthew Marcum <mmarcum@netflixcontractors.com>
2018-09-01 18:53:15 -0500

    [user-auth] fixed test descriptions

--- test/embercatcher_api/account/account_test.exs
+++ test/embercatcher_api/account/account_test.exs
@@ -3,32 +3,36 @@ defmodule EmbercatcherApi.AccountTest do
 
   alias EmbercatcherApi.Account
 
-  describe "users" do
-    alias EmbercatcherApi.Account.User
+  alias EmbercatcherApi.Account.User
 
-    @valid_attrs %{email: "some email", password: "some password"}
-    @update_attrs %{email: "some updated email", password: "some updated password"}
-    @invalid_attrs %{email: nil, password: nil}
+  @valid_attrs %{email: "some email", password: "some password"}
+  @update_attrs %{email: "some updated email", password: "some updated password"}
+  @invalid_attrs %{email: nil, password: nil}
 
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Account.create_user()
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> Account.create_user()
 
-      user
-    end
+    user
+  end
 
-    test "list_users/0 returns all users" do
+  describe "list_users/0" do
+    test "returns all users" do
       user = user_fixture()
       assert Account.list_users() == [user]
     end
+  end
 
+  describe "get_user!/1" do
     test "get_user!/1 returns the user with given id" do
       user = user_fixture()
       assert Account.get_user!(user.id) == user
     end
+  end
 
+  describe "create_user/1" do
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Account.create_user(@valid_attrs)
       assert user.email == @valid_attrs.email
@@ -38,7 +42,9 @@ defmodule EmbercatcherApi.AccountTest do
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Account.create_user(@invalid_attrs)
     end
+  end
 
+  describe "update_user/2" do
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
       assert {:ok, user} = Account.update_user(user, @update_attrs)
@@ -52,13 +58,17 @@ defmodule EmbercatcherApi.AccountTest do
       assert {:error, %Ecto.Changeset{}} = Account.update_user(user, @invalid_attrs)
       assert user == Account.get_user!(user.id)
     end
+  end
 
+  describe "delete_user/1" do
     test "delete_user/1 deletes the user" do
       user = user_fixture()
       assert {:ok, %User{}} = Account.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Account.get_user!(user.id) end
     end
+  end
 
+  describe "change_user/1" do
     test "change_user/1 returns a user changeset" do
       user = user_fixture()
       assert %Ecto.Changeset{} = Account.change_user(user)
